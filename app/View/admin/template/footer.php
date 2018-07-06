c7d3d2e16e6a269869a1867dbd637d36a52bab9e
Diogo Souza <diogo.souza@msn.com>
2018-07-06 01:33:44 -0400

    Dividido template

---
+++ app/View/admin/template/footer.php
@@ -0,0 +1,30 @@
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>Version</b> 2.4.0
+        </div>
+        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        reserved.
+    </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="lib/jquery/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+<!-- Momente -->
+<script src="lib/moment/moment.min.js"></script>
+<!-- FullCalendar -->
+<script src="lib/fullcalendar/fullcalendar.min.js"></script>
+<script src="lib/fullcalendar/locale/pt-br.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/js/adminlte.min.js"></script>
+<!-- Personal -->
+<script src="assets/js/personal.js"></script>
+</body>
+</html>
